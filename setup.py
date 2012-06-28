417d17c0f30db24169322bfed0365f675c7788dc
Kevin McCarthy <me@kevinmccarthy.org>
2012-06-27 16:06:25 -1000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='VCR.py',
+      version='1.0',
+      description='Wordpress Export Parser',
+      author='Kevin McCarthy',
+      author_email='mc@kevinmccarthy.org',
+      packages=['wp_export_parser'],
+)
